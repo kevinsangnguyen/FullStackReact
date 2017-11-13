705ba26db52712ea83c4d7ec2ee3f8ede2455fb7
Kevin Nguyen <kevinnguyen@cg-macbookpro02.local>
2017-11-13 13:42:11 -0800

    set up mongodb and passport

--- services/passport.js
+++ services/passport.js
@@ -1,16 +1,39 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
+const User = mongoose.model('users'); //fetch model class
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); // error, identification
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile:', profile);
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if(existingUser) {
+            //we already have a record with given profile id
+            done(null, existingUser); // user passed to serializeUser
+          } else {
+            // no user record, make a new one
+            new User({ googleId: profile.id }).save().then(user => done(null,user)); // one record async
+          }
+        });
     }
   )
 );
