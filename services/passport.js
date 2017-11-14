34cf1631b5b41dca49d62fbf80650e2e57bb6a38
Kevin Nguyen <kevinnguyen@cg-macbookpro02.local>
2017-11-14 11:15:03 -0800

    react setup w/ dev and prod servers

--- services/passport.js
+++ services/passport.js
@@ -1,21 +1,20 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
 
-const User = mongoose.model('users'); //fetch model class
+const User = mongoose.model("users"); //fetch model class
 
 passport.serializeUser((user, done) => {
-  console.log('serializing');
+  console.log("serializing");
   done(null, user.id); // error, identification
 });
 
 passport.deserializeUser((id, done) => {
-  console.log('deserializing');
-  User.findById(id)
-    .then(user => {
-      done(null, user);
-    })
+  console.log("deserializing");
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
@@ -23,22 +22,16 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-      proxy: true,
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if(existingUser) {
-            //we already have a record with given profile id
-            console.log('existing user');
-            done(null, existingUser); // user passed to serializeUser
-          } else {
-            // no user record, make a new one
-            console.log('new user');
-            new User({ googleId: profile.id }).save().then(user => done(null,user)); // one record async
-          }
-        });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser); // user passed to serializeUser
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
